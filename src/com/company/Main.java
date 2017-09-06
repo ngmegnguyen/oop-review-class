ecd5abfcbc76ff63de83763793e2d80c3d877b0f
BlueRaquel <Raquel.Blue@my.columbiasc.edu>
2017-09-06 15:01:59 -0400

    I did a lot of changes.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         MediaLibrary library = new MediaLibrary();
-        // Add two music objecta to the library
+        // Add two music objects to the library
+        Music CD = new Music("Song", "CD", "Katherine Ngo", 17, 5);
+        Music USB = new Music("Another Song", "Records", "Megan Nguyen", 60, 12);
+
+        library.addMedia(CD);
+        library.addMedia(USB);
 
         // Print the library
 
